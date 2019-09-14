94c8d6a5e0bc62993a153d927bb33593674c3399
Scott Renz <scottrenz@scottrenz.com>
2019-09-14 00:37:15 -0400

    made put and post not allow duplicate names

--- server.js
+++ server.js
@@ -9,8 +9,12 @@ server.use(helmet());
 server.use(express.json());
 
 server.get('/api/projects', (req, res) => {
+  function makebool (item,ix,arr){
+item.completed = item.completed ? true : false
+  }
   db('projects')
   .then(projects => {
+    projects.map(makebool)
     res.status(200).json(projects);
   })
   .catch(error => {
@@ -96,8 +100,14 @@ server.get('/api/projects/tasks/resources/:id', (req, res) => {
 });
 
 server.get('/api/projects/tasks', (req, res) => {
+  function makebool (item,ix,arr){
+    item.project_completed = item.project_completed ? true : false
+    item.task_completed = item.task_completed ? true : false
+      }
+    
   db('project_tasks')
   .then(results => {
+    results.map(makebool)
     res.status(200).json(results);
   })
   .catch(error => {
@@ -115,12 +125,19 @@ server.get('/api/projects/resources', (req, res) => {
 });
 
 server.get('/api/projects/:id/tasks', (req, res) => {
-  db('project_tasks')
+  function makebool (item,ix,arr){
+    item.project_completed = item.project_completed ? true : false
+    item.task_completed = item.task_completed ? true : false
+      }
+    db('project_tasks')
   .where({ project_id: req.params.id })
   .then(results => {
     if(results.length > 0)
-    res.status(200).json(results);
-    else
+{
+  results.map(makebool)
+      res.status(200).json(results);
+}
+      else
     res.status(500).json('invalid project ID');
   })
   .catch(error => {
@@ -129,16 +146,42 @@ server.get('/api/projects/:id/tasks', (req, res) => {
 });
 
 server.put('/api/projects/:id', (req, res) => {
-  console.log('projects body',req.body)
-  db('projects')
-  .update(req.body)
-  .where({ id: req.params.id })
+console.log('put',req.body)
+  let name = req.body.name
+  let description = req.body.description
+  let completed = req.body.completed
+console.log('the 3',name,description,completed)
+  let s = ''
+  if(typeof(name) === 'string')
+{
+  name = name.replace(/'/g,"''")
+  s = ` name = '${name}'`
+}
+  if(typeof(description) === 'string')
+  {
+    description = description.replace(/'/g,"''")
+  s = `${s} ${typeof(name) === 'string' ? ',' : ''}  description = '${description}'`
+  }
+  if(typeof(completed) === 'boolean')
+  s = `${s} ${(typeof(name) === 'string' || typeof(description) === 'string') ? ',' : ''} completed = ${completed ? 1 : 0}`
+console.log(
+  `update projects set ${s} where id = ${req.params.id} and
+  '${name}' not in (select name from projects where id <> ${req.params.id})`)
+
+  db.raw(`update projects set ${s} where id = ${req.params.id} and
+  '${name}' not in (select name from projects where id <> ${req.params.id})`)
   .then(results => {
-    console.log('instru',results,results.length)
-    if(results)
-    res.status(200).json(req.body);
-    else
-    res.status(500).json('invalid project ID');
+    db('projects').where({id: req.params.id})
+    .then(results => {
+      if(results)
+      {
+      results[0].completed = results[0].completed ? true : false  
+      res.status(200).json(results);
+      }
+      else
+      res.status(500).json('invalid update paramater');
+  
+    })
   })
   .catch(error => {
     res.status(500).json(error);
@@ -160,17 +203,30 @@ server.get('/api/projects/:id/resources', (req, res) => {
 });
 
 server.post('/api/projects', (req, res) => {
+db('projects').where({name: req.body.name})
+.then(name => {
+  console.log('name.name body',name,req.body)
+  if(name[0].name === req.body.name)
+  {
+    console.log('name.name',req.body)
+    req.body.name = null
+    console.log('null.name',req.body)
+  }
+})
   db('projects').insert(req.body)
   .then(ids => {
+    console.log('ids',ids,req.body)
     const id = ids[0];
-
     db('projects')
       .where({ id })
       .first()
     .then(project => {
+   project.completed = project.completed ? true : false
       res.status(201).json(project);
-    });
+    })
+  
   })
+
   .catch(error => {
     res.status(500).json(error);
   });
